f7c4b2d652436786abd303ff47ecc9ffb4d65b65
Sergey Kudashkin <sikuda@yandex.ru>
2021-10-01 11:43:35 +0300

    usesCleartextTraffic="true" and view http://www-1c.ru/1

--- app/src/main/java/ru/sikuda/mobile/start32/MainActivity.kt
+++ app/src/main/java/ru/sikuda/mobile/start32/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.btnWeb).setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/mobilesikuda/start32")))
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www-1c.ru/1")))
             }
         })
     }
